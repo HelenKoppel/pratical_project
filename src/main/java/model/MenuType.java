44b0d2ba936d53afad69fe93816c5ef0dea7e11c
GRAFSHEPARD\Admin <repnau.jankristjan@gmail.com>
2021-07-31 11:14:19 +0300

    Created fields, getters and setters and toString for menuType

--- src/main/java/model/MenuType.java
+++ src/main/java/model/MenuType.java
@@ -1,4 +1,52 @@
 package model;
 
+
+import javax.persistence.*;
+
+@Entity
 public class MenuType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_type_id")
+    private int menuTypeId;
+
+    @Column(name = "type_name")
+    private String typeName;
+
+    @Column(name = "description")
+    private String description;
+
+    public int getMenuTypeId() {
+        return menuTypeId;
+    }
+
+    public void setMenuTypeId(int menuTypeId) {
+        this.menuTypeId = menuTypeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuType{" +
+                "menuTypeId=" + menuTypeId +
+                ", typeName='" + typeName + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
