4acd10f7a8fb6f5c4c287d22985fc592e54fa33c
Ragnar Laak <ragnarlaak@gmail.com>
2021-07-31 11:09:43 +0300

    Update OrderDetails.java
    Added attributes to OrderDetails class.

--- src/main/java/model/OrderDetails.java
+++ src/main/java/model/OrderDetails.java
@@ -1,4 +1,24 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
 public class OrderDetails {
+
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int order_details_id;
+
+        @Column(name = "amount")
+        private float amount;
+
+        @Column(name = "no_of_serving")
+        private int no_of_serving;
+
+        @Column(name = "total_amount")
+        private float total_amount;
+
+
 }
