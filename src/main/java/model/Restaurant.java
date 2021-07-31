540be3d63c9cfc64e72df2e2bf2753cd1b66dc14
HelenKoppel <81012100+HelenKoppel@users.noreply.github.com>
2021-07-31 11:05:43 +0300

    Restaurant class

--- src/main/java/model/Restaurant.java
+++ src/main/java/model/Restaurant.java
@@ -1,4 +1,15 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int restaurant;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "address")
+    private String address;
+
 }
