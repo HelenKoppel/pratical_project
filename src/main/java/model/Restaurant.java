11465e9d25ce88a51a9914fccec2babbd0edce8f
HelenKoppel <81012100+HelenKoppel@users.noreply.github.com>
2021-07-31 11:17:47 +0300

    Restaurant class

--- src/main/java/model/Restaurant.java
+++ src/main/java/model/Restaurant.java
@@ -6,10 +6,43 @@ import javax.persistence.*;
 public class Restaurant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "restaurantId")
     private int restaurant;
     @Column(name = "name")
     private String name;
     @Column(name = "address")
     private String address;
 
+    public int getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(int restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "restaurant=" + restaurant +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
